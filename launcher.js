107d0c770bb4273311f860fbc15dd1da3f3bb0d8
stinkeebow <stinkeebow63@gmail.com>
2016-01-10 00:46:46 -0600

    Added handlebars support. Converted API routes to restify

--- launcher.js
+++ launcher.js
@@ -1,9 +1,69 @@
-var servestatic = require("serve-static");
-var express = require("express");
+var restify = require("restify"), handlebars = require("handlebars");
+var fs = require("fs");
 
-var app = express();
+//handlebars elements
+var elements = {
+    "navigation": '<div class="navdiv"><ul class="navigation"><li><a href="/" class="navtext">Home</a></li> <li><a href="/html/aboutus.html" class="navtext">About Us</a></li><li><a href="/projects" class="navtext">Projects</a></li><li><a href="contact" class="navtext">Contact Us</a></li></ul></div>'
+    ,
+    "footer": '<div class="footer"><ul class="addons"><li><a href="http://www.google.com" class="navtext">View Our Source Code</a> </li> <li> <span class="navtext">Made By Hunter and Luke</span></li></ul></div>'
+};
 
-app.use(servestatic(__dirname + "/public"));
-app.listen(3000);
-console.log("launched");
-console.log(__dirname);
+//placeholder for the html files
+var files = {};
+
+var server = restify.createServer({
+    name: "About Us",
+    version: "1.0.0"
+});
+
+server.use(restify.acceptParser(server.acceptable));
+server.use(restify.queryParser());
+server.use(restify.bodyParser());
+
+//initial calling function
+function init(callback) {
+    fs.readFile(__dirname + "/public/html/landing.html", "utf8" , function(err,data) {
+        if(err) {
+            console.log(err);
+        } else {
+            var template = handlebars.compile(data);
+            var result = template(elements);
+            files["landing"] = result;
+        }
+    });
+    fs.readFile(__dirname + "/public/html/aboutus.html", "utf8" , function(err,data) {
+        if(err) {
+            console.log(err);
+        } else {
+            var template = handlebars.compile(data);
+            var result = template(elements);
+            files["aboutus"] = result;
+        }
+    });
+    callback();
+}
+
+init(function() {
+    //set up API routes
+    server.get("/", function(req,res,next) {
+        res.header("Content-Type", "text/html");
+        res.write(files.landing);
+        res.end();
+        return next();
+    });
+
+    server.get("/aboutus", function(req,res,next) {
+        res.header("Content-Type", "text/html");
+        res.write(files.aboutus);
+        res.end();
+        return next();
+    });
+    // catch-all for static content
+    server.get(/.*/, restify.serveStatic({
+        directory: "./public"
+    }));
+
+    server.listen(3000,function() {
+        console.log("launched");
+    });
+});

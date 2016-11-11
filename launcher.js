a6c5cebfa93946e51a8f4e8033cb5e1b40b097cc
Luther Jensen <luther.jensen98@gmail.com>
2016-11-11 13:52:04 -0600

    Added in contact and projects. still not finished

--- launcher.js
+++ launcher.js
@@ -4,16 +4,17 @@ var fs = require("fs");
 
 //handlebars elements
 var elements = {
-    "navigation": '<div class="navdiv"><ul class="navigation"><li><a href="/" class="navtext">Home</a></li> <li><a href="/about" class="navtext">About Me</a></li><li><a href="/projects" class="navtext">Projects</a></li><li><a href="contact" class="navtext">Contact Me</a></li></ul></div>'
+    "navigation": '<div class="navdiv"><ul class="navigation"><li><a href="/" class="navtext">Home</a></li> <li><a href="/about" class="navtext">About Me</a></li><li><a href="/projects" class="navtext">Projects</a></li><li><a href="/contact" class="navtext">Contact Me</a></li></ul></div>'
     ,
     "footer": '<div class="footer"><ul class="addons"><li><a href="https://github.com/warx1a/AboutUs" class="navtext">View The Source Code</a> </li> <li> <span class="navtext">Made By Luke Jensen</span></li></ul></div>'
-};
+    ,
+    "title" : "About Me"};
 
 //placeholder for the html files
 var files = {};
 
 var server = restify.createServer({
-    name: "About Us",
+    name: "About Me",
     version: "1.0.0"
 });
 
@@ -30,6 +31,24 @@ function init(callback) {
             var template = handlebars.compile(data);
             var result = template(elements);
             files["landing"] = result;
+        }
+    });
+    fs.readFile(__dirname + "/public/html/projects.html", "utf8", function(err,data) {
+        if(err) {
+            console.log(err);
+        } else {
+            var template = handlebars.compile(data);
+            var result = template(elements);
+            files["projects"] = result;
+        }
+    });
+    fs.readFile(__dirname + "/public/html/contact.html", "utf8", function(err,data) {
+        if(err) {
+            console.log(err);
+        } else {
+            var template = handlebars.compile(data);
+            var result = template(elements);
+            files["contact"] = result;
         }
     });
     fs.readFile(__dirname + "/public/html/aboutus.html", "utf8" , function(err,data) {
@@ -63,6 +82,20 @@ init(function() {
     server.get("/about", function(req,res,next) {
         res.header("Content-Type", "text/html");
         res.write(files.aboutus);
+        res.end();
+        return next();
+    });
+
+    server.get("/projects", function(req,res,next) {
+        res.header("Content-Type", "text/html");
+        res.write(files.projects);
+        res.end();
+        return next();
+    });
+
+    server.get("/contact", function(req,res,next) {
+        res.header("Content-Type", "text/html");
+        res.write(files.contact);
         res.end();
         return next();
     });
